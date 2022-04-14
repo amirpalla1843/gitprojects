d6a88801962406b32727660ec0eb9bddd37a8d9d
amirm <amirm@DESKTOP-L7O447R>
2022-04-14 23:08:34 +0530

    me try

---
+++ palla/src/main/java/com/springbatch/palla/PallaApplication.java
@@ -0,0 +1,14 @@
+package com.springbatch.palla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PallaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PallaApplication.class, args);
+		System.out.println("git ready");
+		}
+
+}
---
+++ palla/src/test/java/com/springbatch/palla/PallaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.palla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PallaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
